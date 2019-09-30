2e1decc184564a5ca27f076666a75bd9ba503a81
Kyndall Sims <sims.kyndall22@gmail.com>
2019-09-30 15:40:27 -0500

    added student grades

--- src/grades/GradeApplication.java
+++ src/grades/GradeApplication.java
@@ -11,6 +11,23 @@ public class GradeApplication {
         Student student3 = new Student("Luis");
         Student student4 = new Student("Beverly");
 
+        //student 1 grades
+        student1.addGrade(80);
+        student1.addGrade(70);
+        student1.addGrade(90);
+        //student 2 grades
+        student2.addGrade(70);
+        student2.addGrade(77);
+        student2.addGrade(80);
+        //student 3 grades
+        student3.addGrade(80);
+        student3.addGrade(75);
+        student3.addGrade(88);
+        //student 4 grades
+        student4.addGrade(81);
+        student4.addGrade(78);
+        student4.addGrade(91);
+
 
         System.out.println(students);
 
